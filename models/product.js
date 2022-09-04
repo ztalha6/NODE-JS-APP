8271185be1fc21a0959cd34a66f3cc8ccfbf5c66
Muhammad Talha Siddique <ztalha6@gmail.com>
2022-09-04 16:52:20 +0500

    refactoring

--- models/product.js
+++ models/product.js
@@ -1,35 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = callBack => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            callBack([]);
+        } else {
+            callBack(JSON.parse(fileContent));
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) products = JSON.parse(fileContent);
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
-        products.push(this);
     }
 
     static fetchAll(callBack) {
-        const p = path.join(path.dirname(require.main.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) callBack([]);
-            callBack(JSON.parse(fileContent));
-        });
+        getProductsFromFile(callBack);
     }
 }
