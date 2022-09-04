30178c2692989fb290c5910a28e4cb92bd834363
Muhammad Talha Siddique <ztalha6@gmail.com>
2022-09-04 09:58:41 +0500

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,15 @@ exports.postAddProduct = (req, res) => {
 }
 
 exports.getShop = (req, res) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: "Shop",
-        path: "/",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    const products = Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: "Shop",
+            path: "/",
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
+
 }
