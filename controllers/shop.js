9a2ace6ddf0baabf083015931638c56c428016ae
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-01-28 23:55:29 +0500

    getting id from param and loading product details

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,7 +14,9 @@ exports.getProducts = (req, res) => {
 
 exports.getProduct = (req, res) => {
     const prodId = req.params.productId;
-    console.log(prodId);
+    Product.findById(prodId, product => {
+        console.log(product);
+    });
     res.redirect("/");
 }
 
--- models/product.js
+++ models/product.js
@@ -37,4 +37,12 @@ module.exports = class Product {
     static fetchAll(callBack) {
         getProductsFromFile(callBack);
     }
+
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
 }
